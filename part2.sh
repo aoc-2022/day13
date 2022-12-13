f873198a511b47f590a8826dcdc8b28a69f63c56
xeno <xeno@lv427.space>
2022-12-13 20:12:05 +0100

    alternative part 2 improved

--- part2.sh
+++ part2.sh
@@ -1,1 +1,1 @@
-(cat /tmp/aoc/input;echo "[[2]]";echo "[[6]]") | sed 's|\[||g;s|\]|%|g' | sort -n | grep -v "^$" | grep -n "^[26]%%$" | sed "s/:2.*/\*/;s|:.*||" | xargs echo | bc
+(cat /tmp/aoc/input;echo "2]]\n6]]") | sed 's/\[//g;s|\]|%|g' | sort -n | grep -v "^$" | grep -n "^[26]%%$" | sed "s/:2.*/\*/;s|:.*||" | xargs echo | bc
